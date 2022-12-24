b69742d7195b6eb9e040b98f51200add3a76d9db
brioapp <cdrlee@gmail.com>
2022-12-24 11:45:44 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Home';
+
+import './App.css';
+
+const App = () => (
+  <Routes>
+    <Route path="/" element={<Home />} />
+  </Routes>
+);
+
+export default App;
